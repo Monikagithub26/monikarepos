d5297ea1d2a7c8642e3f68663404a2667232cadb
Monika <monsha01@protiviti.com>
2024-10-01 11:28:45 +0530

    my exlipse project

---
+++ src/com/company/session/MyClass.java
@@ -0,0 +1,9 @@
+package com.company.session;
+
+public class MyClass {
+    public static void main(String [] args) {
+    	for(int i=0;i<10;i++) {
+    		System.out.println("Welcome...");
+    	}
+    }
+}
